097f8c885d66a150890ab155adf6efeacddaca4d
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-11-17 21:14:42 +0530

    day 14

--- Day 14 higher lower game/main.py
+++ Day 14 higher lower game/main.py
@@ -4,20 +4,15 @@ from art import logo, vs
 from replit import clear
 
 def get_random_account():
-  """Get data from random account"""
   return random.choice(data)
 
 def format_data(account):
-  """Format account into printable format: name, description and country"""
   name = account["name"]
   description = account["description"]
   country = account["country"]
   return f"{name}, a {description}, from {country}"
 
 def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess 
-  and returns True if they got it right.
-  Or False if they got it wrong.""" 
   if a_followers > b_followers:
     return guess == "a"
   else:
