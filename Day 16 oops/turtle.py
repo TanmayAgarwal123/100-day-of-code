b7d9d06580bcd47d9f99d26d52232f3ffced7001
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-11-26 15:32:02 +0530

    day 16

---
+++ Day 16 oops/turtle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+import random
+
+t=Turtle()
+print(t)
+t.shape("turtle")
+t.color("red")
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
