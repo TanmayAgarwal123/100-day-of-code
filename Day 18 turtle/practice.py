51ef4dd7a59321c00c3e5495a5480f2813ee66b0
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-11-30 23:24:28 +0530

    Update completion status for Day 18

---
+++ Day 18 turtle/practice.py
@@ -0,0 +1,37 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+
+"""
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 10):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
+"""
+"""
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+"""
+"""
+def random_colour():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0,255)
+    random_colour = (r, g, b)
+    return random_colour
+"""
+
