a9bd9a587113104bbd2b34c21b7f35a5d4422798
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-12-04 21:50:52 +0530

    Add high score functionality to scoreboard

--- Day 23 turtle crossing game/scoreboard.py
+++ Day 23 turtle crossing game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.score = 1
+        self.high_score = 1
         self.update_scoreboard()
 
     def update_scoreboard(self):
@@ -22,8 +23,17 @@ class Scoreboard(Turtle):
     def increase_level(self):
         self.score += 1
         self.update_scoreboard()
-    
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("score.txt", mode="w") as score_file:
+                score_file.write(f"{self.high_score}")
+        self.score = 1
+        self.clear()
+        self.update_scoreboard()
+        
+#    def game_over(self):
+#        self.goto(0, 0)
+#        self.write("GAME OVER", align="center", font=("Courier", 24, "normal"))
     
---
+++ Day 24 files, directories and paths/snake project high score/scoreboard.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            #with open("score.txt", mode="w") as score_file:
+            #    score_file.write(f"{self.high_score}")
+        self.score = 0
+        self.clear()
+        self.update_scoreboard()
+    
+    #def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
