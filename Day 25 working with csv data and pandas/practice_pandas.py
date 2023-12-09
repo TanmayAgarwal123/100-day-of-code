64ad45191994e30a0f35e2ae0a2a3f35cf297d9b
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-12-09 21:45:35 +0530

    Delete main.py file

---
+++ Day 25 working with csv data and pandas/practice_pandas.py
@@ -0,0 +1,21 @@
+# Open the weather_data.csv file
+#with open("100 day of code\Day 25 working with csv data and pandas\weather_data.csv") as data_file:
+    # Read all the lines from the file
+#    data = data_file.readlines()
+    # Print the data
+#    print(data)
+
+
+# import csv
+# with open("100 day of code\Day 25 working with csv data and pandas\weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+# import pandas
+
+# data = pandas.read_csv("100 day of code\Day 25 working with csv data and pandas\weather_data.csv")
+# print(data["temp"])
