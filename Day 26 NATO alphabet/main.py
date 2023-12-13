aef51a79ea913551fbaa0e3a455a434049972feb
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-12-13 19:39:57 +0530

    day 26

---
+++ Day 26 NATO alphabet/main.py
@@ -0,0 +1,8 @@
+import pandas
+
+data = pandas.read_csv("100 day of code/Day 26 NATO alphabet/nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
