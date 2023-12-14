e3411f3203c4adfd63b6014c314366e3faf7442c
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2023-12-14 14:51:56 +0530

    Day 27

--- Day 27 Tkinter/practice.py
+++ Day 27 Tkinter/practice.py
@@ -60,9 +60,9 @@ checkbutton.pack()
 def radio_used():
     print(radio_state.get())
     
-radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+#radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1)#, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2)#, variable=radio_state, command=radio_used)
 radiobutton1.pack()
 radiobutton2.pack()
 
