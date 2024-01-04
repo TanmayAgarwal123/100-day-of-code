e62f74b321dc45437abb4a30ad662b4ac66f7fd5
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2024-01-04 20:56:35 +0530

    Add password generator functionality

--- Day 29 Password manager/main.py
+++ Day 29 Password manager/main.py
@@ -1,7 +1,22 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+password_letters = [choice(letters) for item in range(randint(8, 10))]
+password_symbols = [choice(symbols) for item in range(randint(2, 4))]
+password_numbers = [choice(numbers) for item in range(randint(2, 4))]
+
+password_list = password_letters + password_symbols + password_numbers
+shuffle(password_list)
+
+password = "".join(password_list)
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
