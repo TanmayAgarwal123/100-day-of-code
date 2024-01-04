7f83332b69765421ff682dcf3dc37d247f53d358
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2024-01-04 21:04:50 +0530

    Add pyperclip import for password copying

--- Day 29 Password manager/main.py
+++ Day 29 Password manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
