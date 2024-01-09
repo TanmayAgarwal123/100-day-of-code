9ef61693ea2b0e32d26f75d09cfaadf6e07afa25
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2024-01-09 12:05:12 +0530

    Add sunset time functionality

--- Day 33 ISS tracker/sunset.py
+++ Day 33 ISS tracker/sunset.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import *
 MY_LAT = 25.218501
 MY_LONG = 75.879103
 
@@ -8,6 +9,13 @@ parameters = {
     "formatted": 0,
 }
 
-response = requests.get(url="https://api.sunrise-sunset.org/json")
+response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameters)
 response.raise_for_status()
-print(response.json())
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+time_now = datetime.now()
+print(time_now.hour)
