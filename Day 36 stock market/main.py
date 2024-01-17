4a5a7c0698f24dbe26f219a08c04454d820e82a3
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2024-01-17 11:42:44 +0530

    Add Twilio integration and format article messages

--- Day 36 stock market/main.py
+++ Day 36 stock market/main.py
@@ -1,4 +1,9 @@
 import requests
+#from twilio.rest import Client
+
+
+# VIRTUAL_TWILIO_NUMBER = "your virtual twilio number"
+# VERIFIED_NUMBER = "your own phone number verified with Twilio"
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -65,19 +70,17 @@ if diff_percent > 1:
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articals]
+print(formatted_articles)
 
 #TODO 9. - Send each article as a separate message via Twilio. 
 
+# client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+# for article in formatted_articles:
+#     message = client.messages.create(
+#         body=article,
+#         from_=VIRTUAL_TWILIO_NUMBER,
+#         to=VERIFIED_NUMBER
+#     )
 
 
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
