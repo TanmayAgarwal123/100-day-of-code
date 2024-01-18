06399e52df657bc89e7cbd78c5261a2ae6529f06
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2024-01-18 06:07:59 +0530

    Update pixel_data quantity with user input

--- Day 37 habit tracking/main.py
+++ Day 37 habit tracking/main.py
@@ -37,16 +37,16 @@ today = datetime.now()
 
 pixel_data = {
     "date": today.strftime("%Y%m%d"),
-    "quantity": "4.2",
+    "quantity": input("How many hours did you code today? "),
 }
 
-# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
-# print(response.text)
-
-update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-new_pixel_data = {
-    "quantity": "4.5",
-}
-
-response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
+
+# update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# new_pixel_data = {
+#     "quantity": "4.5",
+# }
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
