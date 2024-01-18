46c48b4316ae57ec361145ff3640c4efb8877ec6
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2024-01-18 06:01:54 +0530

    Add pixel creation functionality to main.py

--- Day 37 habit tracking/main.py
+++ Day 37 habit tracking/main.py
@@ -1,7 +1,10 @@
 import requests
+from datetime import datetime
 
 USERNAME = "tanmay10"
 TOKEN = "tanmayagarwal"
+GRAPH_ID = "graph1"
+#website = https://pixe.la/v1/users/tanmay10/graphs/graph1.html
 
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
@@ -15,10 +18,28 @@ user_params = {
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "hours",
     "type": "float",
     "color": "ajisai",
 }
-requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "4.2",
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
