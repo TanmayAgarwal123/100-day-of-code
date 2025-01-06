babacca4d15fc2c396621a548a92599ed0a601fe
TanmayAgarwal123 <tanmay.agarwal2021@vitstudent.ac.in>
2025-01-06 18:44:05 +0530

    day 1

---
+++ Day 1/main.py
@@ -0,0 +1,6 @@
+#path: 100%20day%20of%20code/Day%201%20band%20name%20generator/main.py
+print("Welcome to the band name generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet + ".")
+
---
+++ Day 1/text.md
@@ -0,0 +1,1 @@
+it was all about basics and resources, no project was actually made.
